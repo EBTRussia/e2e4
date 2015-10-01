446ae1c7fe443f42fd1ecf8808d6094b8a0f5f3e
Fedor Shchudlo <fshchudlo@gmail.com>
2015-10-01 17:19:49 +0600

    jquery.newUUID deleted

--- src/globalConfigurator.js
+++ src/globalConfigurator.js
@@ -276,18 +276,6 @@
             };
             def.extendjQuery = function ()
             {
-                jquery.newUUID = function ()
-                {
-                    var d = new Date().getTime();
-                    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c)
-                    {
-                        var r = (d + Math.random() * 16) % 16 | 0;
-                        d = Math.floor(d / 16);
-                        return (c === 'x' ? r : (r & 0x7 | 0x8)).toString(16);
-                    });
-                    return uuid;
-                };
-
                 jquery.fn.mobileFix = function (options)
                 {
                     var $parent = jquery(this);
