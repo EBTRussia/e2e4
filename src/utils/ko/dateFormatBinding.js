f563f0c11102047ad8780b82e710a7262dd1e0bd
Fedor Shchudlo <fshchudlo@gmail.com>
2015-10-05 11:30:36 +0600

    separate dateFormatBinding module

---
+++ src/utils/ko/dateFormatBinding.js
@@ -0,0 +1,41 @@
+﻿define(['ko', 'jquery', 'moment'], function (ko, jquery, moment)
+{
+    'use strict';
+    ko.bindingHandlers.dateFormat = {
+        after: ['value', 'text'],
+        update: function (element, valueAccessor, allBindings)
+        {
+            var format = ko.unwrap(valueAccessor()) || 'L';
+            var value;
+            var text;
+            var d;
+            if (allBindings.get('value'))
+            {
+
+                value = ko.unwrap(allBindings.get('value'));
+                if (value)
+                {
+                    d = moment(value);
+                    text = d.isValid() ? d.format(format) : value;
+                } else
+                {
+                    text = '';
+                }
+                jquery(element).val(text);
+            }
+            if (allBindings.get('text'))
+            {
+                value = ko.unwrap(allBindings.get('text'));
+                if (value)
+                {
+                    d = moment(value);
+                    text = d.isValid() ? d.format(format) : value;
+                } else
+                {
+                    text = '';
+                }
+                jquery(element).text(text);
+            }
+        }
+    };
+});
--- src/utils/ko/formatBindings.js
+++ src/utils/ko/formatBindings.js
@@ -1,44 +1,6 @@
-﻿define(['ko', 'jquery', 'moment'], function (ko, jquery, moment)
+﻿define(['ko', 'jquery'], function (ko, jquery)
 {
     'use strict';
-    ko.bindingHandlers.dateFormat = {
-        after: ['value', 'text'],
-        update: function (element, valueAccessor, allBindings)
-        {
-            var format = ko.unwrap(valueAccessor()) || 'L';
-            var value;
-            var text;
-            var d;
-            if (allBindings.get('value'))
-            {
-
-                value = ko.unwrap(allBindings.get('value'));
-                if (value)
-                {
-                    d = moment(value);
-                    text = d.isValid() ? d.format(format) : value;
-                } else
-                {
-                    text = '';
-                }
-                jquery(element).val(text);
-            }
-            if (allBindings.get('text'))
-            {
-                value = ko.unwrap(allBindings.get('text'));
-                if (value)
-                {
-                    d = moment(value);
-                    text = d.isValid() ? d.format(format) : value;
-                } else
-                {
-                    text = '';
-                }
-                jquery(element).text(text);
-            }
-        }
-    };
-    
     ko.bindingHandlers.booleanText = {
         update: function (element, valueAccessor)
         {
