c1f98ef39442df82ec7219dd030dd6be4e2032b6
Ivan Grunev <i.grunev@eastbanctech.ru>
2016-07-28 10:22:08 +0600

    Добавил динамическую смену сообщений валидации Добавил возможность менять параметр длины поля

--- src/utils/ko/customValidationRules.js
+++ src/utils/ko/customValidationRules.js
@@ -102,5 +102,51 @@
             },
             message: 'This field is required.'
         };
+        
+        ko.validation.rules['minLength'] = {
+            validator: function (val, minLength) {
+                if(ko.validation.utils.isEmptyVal(val)) { return true; }
+                var minLengthUW = ko.unwrap(minLength);
+                var normalizedVal = ko.validation.utils.isNumber(val) ? ('' + val) : val;
+                return normalizedVal.length >= minLengthUW;
+            },
+            message: 'Please enter at least {0} characters.'
+        };
+
+        ko.validation.rules['minLengthOrMaxLength'] = {
+            validator: function (val, length) {
+                if(ko.validation.utils.isEmptyVal(val)) { return true; }
+                var lengthUW = ko.unwrap(length);
+                var normalizedVal = ko.validation.utils.isNumber(val) ? ('' + val) : val;
+                return (normalizedVal.length == lengthUW[0] || normalizedVal.length == lengthUW[1]);
+            },
+            message: 'Please enter at least {0} characters.'
+        };
+        
+        ko.validation.formatMessage = function (message, params, observable) {
+            if (ko.validation.utils.isObject(params) && params.typeAttr) {
+                params = params.value;
+            }
+            if (ko.isObservable(message)) {
+                message = ko.unwrap(message);
+            }
+            if (typeof message === 'function') {
+                return message(params, observable);
+            }
+            var replacements = ko.unwrap(params);
+            if (replacements == null) {
+                replacements = [];
+            }
+            if (!ko.validation.utils.isArray(replacements)) {
+                replacements = [replacements];
+            }
+            return message.replace(/{(\d+)}/gi, function(match, index) {
+                if (typeof replacements[index] !== 'undefined') {
+                    return replacements[index];
+                }
+                return match;
+            });
+        };
+    
         ko.validation.registerExtenders();
     });
